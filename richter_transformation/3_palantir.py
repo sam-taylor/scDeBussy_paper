3902f72ce8f7c1bc4aa7e66788106cbf27250415
Mamie Wang <wangm10@mskcc.org>
2025-01-28 14:29:41 -0500

    Changed to logger and fixed a error in extracting obs column

--- richter_transformation/3_palantir.py
+++ richter_transformation/3_palantir.py
@@ -7,7 +7,9 @@ from scipy.stats import pearsonr
 import palantir
 import scvi
 import torch
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def determine_terminal_cells(adata_patient, cell_type, dm_mat, annotation_column):
     init_pdt = adata_patient.obs[annotation_column].map(lambda x: 1 if x == cell_type else 0)
@@ -19,17 +21,17 @@ def determine_terminal_cells(adata_patient, cell_type, dm_mat, annotation_column
     return start_cell_bc
 
 def run_palantir_analysis(adata, start_cell_type, end_cell_type, cell_type_col, save_path):
-    print("Preparing DM matrix...")
+    logging.info("Preparing DM matrix...")
     dm_key = 'DM_EigenVectors'
     dm_mat = pd.DataFrame(adata.obsm[dm_key], index=adata.obs.index)
     
-    print("Determining start cell...")
+    logging.info("Determining start cell...")
     start_cell_bc = determine_terminal_cells(adata, start_cell_type, dm_mat, cell_type_col)
     
-    print("Determining end cell...")
+    logging.info("Determining end cell...")
     end_cell_bc = determine_terminal_cells(adata, end_cell_type, dm_mat, cell_type_col)
     
-    print("Running Palantir...")
+    logging.info("Running Palantir...")
     terminal_states = pd.Series([end_cell_type], index=[end_cell_bc])
     pr_res = palantir.core.run_palantir(
         dm_mat, start_cell_bc, terminal_states=terminal_states, use_early_cell_as_start=True
@@ -42,30 +44,32 @@ def run_cell_aligndtw(path, batch_key, start_cell_type, end_cell_type, cell_type
     adata = sc.read_h5ad(path)
     adata.X = adata.layers['counts'].copy()
     patients = adata.obs.loc[:,batch_key].unique()
-    n_components = [10, 10, 10, 10, 10]
+    n_component = 15
+    n_top_genes = 2000
+    n_neighbors = 100
 
     for i, patient in enumerate(patients):
-        print(patient)
+        logging.info(f'Processing sample {patient}')
         adata_patient = adata[adata.obs.loc[:,batch_key] == patient].copy()
-        print("Filtering highly variable genes...")
-        sc.pp.highly_variable_genes(adata_patient, n_top_genes=10000, flavor='seurat_v3')
+        logging.info("Filtering highly variable genes...")
+        sc.pp.highly_variable_genes(adata_patient, n_top_genes=n_top_genes, flavor='seurat_v3')
         sc.pp.normalize_total(adata_patient, target_sum=1e4)
         sc.pp.log1p(adata_patient)
-        print("Running PCA...")
+        logging.info("Running PCA...")
         sc.pp.pca(adata_patient)
-        print("Computing neighbors...")
-        sc.pp.neighbors(adata_patient)
-        print("Running UMAP...")
+        logging.info("Computing neighbors...")
+        sc.pp.neighbors(adata_patient, n_neighbors=n_neighbors)
+        logging.info("Running UMAP...")
         sc.tl.umap(adata_patient)
-        print("Running PAGA...")
+        logging.info("Running PAGA...")
         sc.tl.paga(adata_patient, groups=cell_type_col)
         sc.pl.paga(adata_patient, plot=True)
-        print("Drawing graph...")
+        logging.info("Drawing graph...")
         sc.tl.draw_graph(adata_patient, init_pos='paga')
-        print("Running Magic Imputation...")
-        dm_res = palantir.utils.run_diffusion_maps(adata_patient, n_components=n_components[i])
+        logging.info("Running Magic Imputation...")
+        dm_res = palantir.utils.run_diffusion_maps(adata_patient, n_components=n_component)
         imputed_X = palantir.utils.run_magic_imputation(adata_patient)
-        print("Saving tumor cells adata for ", patient)
+        logging.info(f"Saving tumor cells adata for {patient}")
         adata_patient.write_h5ad(os.path.join(os.path.dirname(path), f'{patient}.h5ad'))
         outpath = os.path.join(os.path.dirname(path), f'{patient}_palantir.h5ad')
         adata_patient = run_palantir_analysis(adata_patient, start_cell_type, end_cell_type, cell_type_col, outpath)
@@ -75,17 +79,17 @@ def run_harmony(path, batch_key, start_cell_type, end_cell_type, cell_type_col):
     adata = sc.read_h5ad(path)
     sc.external.pp.harmony_integrate(adata, batch_key, basis="X_pca")
     sc.pp.neighbors(adata, use_rep="X_pca_harmony")
-    print("Running UMAP...")
+    logging.info("Running UMAP...")
     sc.tl.umap(adata)
-    print("Running PAGA...")
+    logging.info("Running PAGA...")
     sc.tl.paga(adata, groups=cell_type_col)
     sc.pl.paga(adata, plot=True)
-    print("Drawing graph with Harmony...")
+    logging.info("Drawing graph with Harmony...")
     sc.tl.draw_graph(adata, use_rep='X_pca_harmony', init_pos='paga')
-    print("Running Magic Imputation...")
+    logging.info("Running Magic Imputation...")
     dm_res = palantir.utils.run_diffusion_maps(adata, n_components=10, pca_key='X_pca_harmony')
     imputed_X = palantir.utils.run_magic_imputation(adata)
-    print("Saving tumor cells adata to path:", path)
+    logging.info(f"Saving tumor cells adata to path: {path}")
     adata.write_h5ad(os.path.join(os.path.dirname(path), 'harmony_combined.h5ad'))
     outpath = os.path.join(os.path.dirname(path), f'harmony_palantir.h5ad')
     adata = run_palantir_analysis(adata, start_cell_type, end_cell_type, cell_type_col, outpath)
@@ -101,25 +105,25 @@ def run_scvi(path, batch_key, start_cell_type, end_cell_type, cell_type_col):
     latent = model.get_latent_representation()
     denoised = model.get_normalized_expression(library_size=1e4)
     integrated = model.get_normalized_expression(
-    transform_batch=adata.obs[:,batch_key].unique().tolist(), library_size=1e4
+    transform_batch=adata.obs[batch_key].unique().tolist(), library_size=1e4
     )
     adata.obsm["X_scVI"] = latent
     adata.obsm["scvi_normalized"] = denoised
     adata.obsm["scvi_normalized_integrated"] = integrated
     
-    print("Computing neighbors with scVI...")
+    logging.info("Computing neighbors with scVI...")
     sc.pp.neighbors(adata, use_rep="X_scVI")
-    print("Running UMAP...")
+    logging.info("Running UMAP...")
     sc.tl.umap(adata)
-    print("Running PAGA...")
+    logging.info("Running PAGA...")
     sc.tl.paga(adata, groups=cell_type_col)
     sc.pl.paga(adata, plot=True)
-    print("Drawing graph with Harmony...")
+    logging.info("Drawing graph with Harmony...")
     sc.tl.draw_graph(adata, use_rep='X_scVI', init_pos='paga')
-    print("Running Magic Imputation...")
+    logging.info("Running Magic Imputation...")
     dm_res = palantir.utils.run_diffusion_maps(adata, n_components=10, pca_key='X_scVI')
     imputed_X = palantir.utils.run_magic_imputation(adata)
-    print("Saving results...")
+    logging.info("Saving results...")
     adata.write_h5ad(os.path.join(os.path.dirname(path), 'scvi_combined.h5ad'))
     outpath = os.path.join(os.path.dirname(path), f'scvi_palantir.h5ad')
     adata = run_palantir_analysis(adata, start_cell_type, end_cell_type, cell_type_col, outpath)
