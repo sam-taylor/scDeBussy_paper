4a49b7b1d0c27a50a425d65a04fc52fde10eba9d
Mamie Wang <wangm10@mskcc.org>
2025-01-29 14:55:39 -0500

    Added data preprocessing code for B cell development

--- README.md
+++ README.md
@@ -3,11 +3,15 @@ Code to reproduce the figures in the paper
 
 
 ## B cell development
+The source data of bone marrow B cell development came from [Human Cell Atlas](https://explore.data.humancellatlas.org/projects/cc95ff89-2e68-4a08-a234-480eca21ce79/get-curl-command). We downloaded the data in h5ad format and the relevant file is `20492a4b-0def-457b-9574-60dfdde2a0f2/BM_standard_design.h5ad`. We extracted three B cells subsets: Pre-B, Pro-B, and Naive B cells. 
 
+The scripts that are used to extract and preprocess data are located in folder `b_cell_development`, as described in the following table. 
 
-
-## COVID19 
-
+| Script            | Description                                               |
+|-------------------|-----------------------------------------------------------|
+| 1_extract_data.py | Extract the B cells and preprocess the source data        |
+| 2_palantir.py     | Run palantir pseudotime on h5ad files                     |
+| 3_align.py        | Align pseudotime across samples                           |
 
 
 ## Richter transformation
@@ -29,8 +33,4 @@ The scripts that are used to extract and preprocess data are located in folder `
 | 2_preprocess.py  | Preprocess the expression matrix into h5ad file           |
 | 3_palantir.py    | Run palantir pseudotime on h5ad files                     |
 | 4_align.py       | Align pseudotime across samples                           |
-
-
-## Downstream analysis
-
 
---
+++ b_cell_development/3_align.py
@@ -0,0 +1,49 @@
+import scanpy as sc
+import pandas as pd
+import os
+import numpy as np
+from CellAlignDTW.pp import create_cellrank_probability_df
+from CellAlignDTW import CellAlignDTW, gam_smooth_expression
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+path = "../data/HCA-BM/results/"
+patients = ['1', '2', '3', '4', '5', '6', '7', '8']
+cluster_ordering = ['Pro-B cells', 'Pre-B cells', 'Naive B cells']
+df_combined = pd.DataFrame()
+
+h5ad_files = [os.path.join(path, f"{patient}_palantir.h5ad") for patient in patients]
+logger.info("Starting to create cellrank probability dataframe...")
+combined_adata, df = create_cellrank_probability_df(h5ad_files, 'anno', patients, cluster_ordering, 'palantir_pseudotime')
+logger.info("Cellrank probability dataframe created successfully.")
+
+aligner = CellAlignDTW(df=df, 
+                       cluster_ordering=cluster_ordering,
+                       subject_col='subject',
+                       score_col='score',
+                       cell_id_col='cell_id',
+                       cell_type_col='cell_type',
+                       verbose=True)
+logger.info("Starting alignment process...")
+aligner.align()
+logger.info("Alignment process completed.")
+
+combined_adata.write_h5ad(os.path.join(path, 'combined_palantir.h5ad'))
+logger.info("Combined adata written to file.")
+
+aligner.df.to_csv(os.path.join(path, 'aligned_pseudotime.csv'), index=False)
+logger.info("Aligned pseudotime dataframe written to file.")
+
+logger.info("Starting GAM smoothing expression...")
+df = aligner.df
+combined_adata = sc.read_h5ad(os.path.join(path, 'combined_palantir.h5ad'))
+genes = combined_adata.var_names.values.tolist()
+summary_df, gene_curves, scores_df = gam_smooth_expression(df, genes, n_splines=6, lam=4)
+logger.info("GAM smoothing expression completed.")
+
+summary_df.to_csv(os.path.join(path, 'gam_summary.csv'), index=False)
+gene_curves.to_csv(os.path.join(path, 'gam_gene_curves.csv'), index=False)
+scores_df.to_csv(os.path.join(path, 'gam_scores.csv'), index=False)
