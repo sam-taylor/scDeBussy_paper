2d9536e10f1673326352cc81d2e7818fa12333a7
Mamie Wang <wangm10@mskcc.org>
2025-02-20 14:55:38 -0500

    Added code to convert simulated data to anndata

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,5 +1,8 @@
+# The simulation is based on Genes2Genes Gaussian Process simulation for pseudotime data (matching case)
+# https://github.com/Teichlab/G2G_notebooks/blob/main/Simulation_experiments/DataGeneration_TrajectorySimulatorUsingGPs.ipynb
 import torch
 import os
+import anndata
 import gpytorch
 import seaborn as sb
 import numpy as np
@@ -7,6 +10,8 @@ import matplotlib.pyplot as plt
 import warnings
 import pickle
 from tqdm import tqdm
+import pandas as pd
+import scipy
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 torch.cuda.is_available() 
@@ -54,6 +59,49 @@ def run_all_match_simulator(T_f, n_simulations, base_mean_factor=0.0):
     return matched_datasets
 
 
+def convert2adata(simulated_datasets,T_f, write=False):
+    # if const_std mode, G2G uses a constant of std=0.1 for all time points 
+    n_sim_genes = len(simulated_datasets) 
+    sample_1_df = []
+    sample_2_df = []
+    sample_3_df = []
+    time = T_f.detach().numpy()
+    
+    for dataset in simulated_datasets:
+        sample_1_df.append( dataset[0])
+        sample_2_df.append( dataset[1])
+        sample_3_df.append( dataset[2])
+
+    sample_1_df = pd.DataFrame(sample_1_df).transpose() 
+    sample_2_df = pd.DataFrame(sample_2_df).transpose() 
+    sample_3_df = pd.DataFrame(sample_3_df).transpose() 
+    sample_1_df.columns = np.asarray(['Gene' + str(x) for x in np.arange(n_sim_genes)]) 
+    sample_2_df.columns = sample_1_df.columns
+    sample_3_df.columns = sample_1_df.columns
+    gene_list = list(sample_1_df.columns)
+
+    adata_sample_1 = anndata.AnnData(X=scipy.sparse.csr_matrix((sample_1_df)))
+    adata_sample_1.var_names = sample_1_df.columns
+    adata_sample_1.obs_names = sample_1_df.index
+    adata_sample_1.obs['time'] = time
+    adata_sample_2 = anndata.AnnData(X=scipy.sparse.csr_matrix(sample_2_df))
+    adata_sample_2.var_names = sample_2_df.columns
+    adata_sample_2.obs_names = sample_2_df.index
+    adata_sample_2.obs['time'] = time
+    adata_sample_3 = anndata.AnnData(X=scipy.sparse.csr_matrix(sample_3_df))
+    adata_sample_3.var_names = sample_3_df.columns
+    adata_sample_3.obs_names = sample_3_df.index
+    adata_sample_3.obs['time'] = time
+    
+    if(write):
+        dataset_dir = 'data/simulation/'
+        adata_sample_1.write_h5ad(  dataset_dir   + 'sample_1' +'.h5ad')
+        adata_sample_2.write_h5ad(dataset_dir   + 'sample_2' +'.h5ad')
+        adata_sample_3.write_h5ad(dataset_dir   + 'sample_3' +'.h5ad')
+
+    return adata_sample_1, adata_sample_2, adata_sample_3
+
+
 torch.manual_seed(1) 
 np.random.seed(1)
 T_f = torch.tensor(np.linspace(0,1,300,dtype=np.float64))
@@ -62,3 +110,5 @@ plot_simulated_dataset(matched_datasets[10:26], T_f, figsize=(15,10))
 if not os.path.exists('data/simulation'):
     os.makedirs('data/simulation')
 pickle.dump(matched_datasets, open('data/simulation/matched_datasets.pkl', 'wb'))
+
+adata_sample_1, adata_sample_2, adata_sample_3 = convert2adata(matched_datasets, T_f, write=False)
